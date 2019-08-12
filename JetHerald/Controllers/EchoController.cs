ff70cda27a11d41726a3eb1e52a938d9a45cbc27
jetsparrow <scram.jet.sparrow@gmail.com>
2019-08-12 21:20:22 +0300

    Debug echo controller

---
+++ JetHerald/Controllers/EchoController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JetHerald.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get(string msg)
+        {
+            await Task.Delay(100);
+            return Ok(msg);
+        }
+
+        [HttpPost]
+        public IActionResult GetSync(string msg)
+        {
+            return Ok(msg);
+        }
+    }
+}
